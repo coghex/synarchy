b4db44fa18578d12dd52d6c70481ede3771d4571
coghex <vincecoghlan@gmail.com>
2026-02-02 10:16:07 -0800

    cleanup

--- src/Engine/Core/Base.hs
+++
@@ -1,9 +0,0 @@
-module Engine.Core.Base where
-
-import UPrelude
-import Engine.Core.Var
-import qualified Control.Monad.Logger.CallStack as Logger
-
-
-type LoggingFunc = Logger.Loc → Logger.LogSource → Logger.LogLevel
-                              → Logger.LogStr → IO ()
