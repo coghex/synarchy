8f3869d68ae8faa4941d88f18cf407f2e4694369
coghex <vincecoghlan@gmail.com>
2026-02-20 12:30:09 -0800

    dead code gone

--- src/World/Hydrology/River.hs
+++ src/World/Hydrology/River.hs
@@ -1,12 +1,10 @@
 {-# LANGUAGE Strict, UnicodeSyntax #-}
 module World.Hydrology.River
-    ( evolveRiver
-    , applyRiverCarve
+    ( applyRiverCarve
     , applyRiverEvolution
     , carveFromSegment
     , computeDeltaDeposit'
     ) where
 
-import World.Hydrology.River.Evolution (evolveRiver)
 import World.Hydrology.River.Carving (applyRiverCarve, applyRiverEvolution
                                      , carveFromSegment, computeDeltaDeposit')
