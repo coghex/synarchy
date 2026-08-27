3716a8d81ebdc55861b031469777e03841060430
coghex <vincecoghlan@gmail.com>
2026-08-27 06:29:08 -0700

    test: keep preview probe from stealing focus

--- test-headless/Spec.hs
+++ test-headless/Spec.hs
@@ -202,6 +202,7 @@ import qualified Test.Headless.Core.WorkerLifecycle as WorkerLifecycle
 import qualified Test.Headless.Core.DebugListener as DebugListener
 import qualified Test.Headless.App.Cli as AppCli
 import qualified Test.Headless.App.ChunkRegion as AppChunkRegion
+import qualified Test.Headless.App.PreviewConfig as PreviewConfig
 import qualified Test.Headless.Camera.GotoClamp as GotoClamp
 import qualified Test.Headless.Camera.ZoomScroll as ZoomScroll
 import qualified Test.Headless.Scene.BatchMerge as BatchMerge
@@ -653,6 +654,7 @@ main = hspec $ do
     DebugListener.spec
     AppCli.spec
     AppChunkRegion.spec
+    describe "App.Preview.Config" PreviewConfig.spec
     describe "Camera.GotoClamp" GotoClamp.spec
     describe "Camera.ZoomScroll" ZoomScroll.spec
     describe "Scene.BatchMerge" BatchMerge.spec
